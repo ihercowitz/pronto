52b32a221124de5f8a55b3f066f1da69b259793e
Igor Hercowitz <ihercowitz@gmail.com>
2019-06-27 20:02:13 +0200

    Fixed boolean question

--- front/scripts/components/Answer.vue
+++ front/scripts/components/Answer.vue
@@ -1,20 +1,26 @@
 <template>
-   
-   <b-button-group>
+  <div> 
+   <b-button-group v-if="!isBoolean">
       <b-button @click="setSelection('')" :class="{ 'btn-success': '' === selectedValue }">Em branco</b-button>
       <b-button v-for="(item, index) in 4" :key="index" @click="setSelection(index)" :class="{ 'btn-success': index === selectedValue }">{{ item }}</b-button>
     </b-button-group>
-
   
+     <b-button-group v-if="isBoolean">
+      <b-button @click="setSelection('')" :class="{ 'btn-success': '' === selectedValue }">Em branco</b-button>
+      <b-button :key="yes" @click="setSelection('Y')" :class="{ 'btn-success': 'Y' === selectedValue }">Sim</b-button>
+      <b-button :key="yes" @click="setSelection('N')" :class="{ 'btn-success': 'N' === selectedValue }">Nao</b-button>
+    </b-button-group>
+  </div>
 
 </template>
 
 <script>
 export default {
   name: 'answer',
-  props: ['selected'],
+  props: ['selected', 'isBoolean'],
   data() {
     return {
+
       selectedValue: this.selected
     }
   },
